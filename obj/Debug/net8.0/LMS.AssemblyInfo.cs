e2d0b4f7ccd76da47b9a0c10fb79cd65c589e643
zaidmzs <muhammadzaid.mzs@gmail.com>
2024-04-21 17:09:44 +0500

    Adding Images

--- obj/Debug/net8.0/LMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/LMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ff353ed248b59fa98cb0887d171b41df68966f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c310e47ea5ce8e5802a030b58431fc4ffb27d3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
